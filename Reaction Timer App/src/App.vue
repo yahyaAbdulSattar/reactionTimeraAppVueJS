6f99ca9a63b7685ddd2d8fa20ccde01bec948486
yahya <yahya.abdulsattar2003@gmail.com>
2023-10-01 14:17:39 +0530

    This is my project for learning vue js

---
+++ Reaction Timer App/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <body>
+    <h1>Ninja Reaction Timer</h1>
+    <button @click="start" :disabled="isPlaying">play</button>
+      <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+      <Result v-if="showResults" :score="score"/>
+        
+      
+  </body>
+  
+</template>
+
+<script>
+  import Block from './components/Block.vue'
+  import Result from './components/Results.vue'
+  export default {
+    name: 'App',
+    components: { Block, Result },
+    data(){
+      return{
+        isPlaying: false,
+        delay: null,
+        score: null,
+        showResults: false
+        
+      }
+    },
+    methods: {
+      start(){
+        this.delay = 2000 + Math.random() * 5000,
+        this.isPlaying = !this.isPlaying
+        this.showResults = false
+      },
+      endGame(reactionTime){
+        this.score = reactionTime
+        this.isPlaying = false
+        this.showResults = true
+      }
+    }
+  }
+</script>
+<style scoped>
+  body{
+    text-align: center;
+  }
+  
+  
+</style>
